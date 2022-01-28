fb3ef8f49c96d261133565768b8f049f87180496
Tanya Rushing <tanyarushing@Tanyas-Air.lan>
2022-01-27 20:31:46 -0500

    first commit

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
 let content = document.querySelector('.js-generated.content')
+let header = document.createElement('h1')  
+header.setAttribute('class','dog-name')
+header.append('Rizzo')
+content.append(header) 
+//append document image
+let dogImage = document.createElement('img')
+dogImage.setAttribute('alt',"Dog Image")
+dogImage.setAttribute('src',"./assets/rizzo.jpg")
+dogImage.setAttribute('width',"220")
+content.append(dogImage)
+// Add Details
+let dogDetails = document.queryElement('h3','p')
+dogDetails[0].setAttribute.
