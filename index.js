c026c611cb5229e49a3e8ddf185abde8c72c0226
Tanya Rushing <tanyarushing@Tanyas-Air.lan>
2022-01-31 18:55:09 -0500

    first commit

--- index.js
+++ index.js
@@ -1,14 +1,37 @@
-let content = document.querySelector('.js-generated.content')
-let header = document.createElement('h1')  
-header.setAttribute('class','dog-name')
-header.append('Rizzo')
-content.append(header) 
-//append document image
-let dogImage = document.createElement('img')
-dogImage.setAttribute('alt',"Dog Image")
-dogImage.setAttribute('src',"./assets/rizzo.jpg")
-dogImage.setAttribute('width',"220")
-content.append(dogImage)
-// Add Details
-let dogDetails = document.queryElement('h3','p')
-dogDetails[0].setAttribute.
+let content = document.querySelector(".js-generated.content");
+let header = document.createElement('h1');
+header.setAttribute('class',"dog-name");
+header.append("Rizzo");
+content.append(header);
+
+let dogContent = document.createElement('div');
+dogContent.setAttribute('class','dog-content');
+content.append(dogContent);
+
+let dogImage =document.createElement(img);
+dogImage.setAttribute('class', 'dog-image');
+dogImage.setAttribute('src', "./assets/rizzo.jpg");
+dogContent.append(dogImage);
+
+let dogDetails = document.createElement("div");
+dogDetails.setAttribute("class", "dog-details");
+dogContent.append(dogDetails);
+let description = document.createElement("h3");
+description.textContent = "Description";
+dogDetails.append(description);
+let paragraph = document.createElement("p");
+paragraph.textContent =
+  "This gentle dog is aloof toward her owner, and never comes when called. She always acts as though any stranger she meets will harm her, and dislikes other animals.";
+dogDetails.append(paragraph);
+let feedingTimes = document.createElement("h3");
+feedingTimes.textContent = "Feeding Times:";
+dogDetails.append(feedingTimes);
+let unorderedList = document.createElement("ul");
+let item1 = document.createElement("li");
+let item2 = document.createElement("li");
+let item3 = document.createElement("li");
+item1.textContent = "9:00 am";
+item2.textContent = "12:00 pm";
+item3.textContent = "5:00 pm";
+dogDetails.append(unorderedList);
+unorderedList.append(item1, item2, item3);
